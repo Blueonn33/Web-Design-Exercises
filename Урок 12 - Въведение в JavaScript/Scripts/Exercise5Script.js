d9bb7280bef990d311b5f2f4c73633cf748816aa
Martin Marinov <m.marinov1733@gmail.com>
2025-08-11 16:19:17 +0300

    fizzbuzz

---
+++ Урок 12 - Въведение в JavaScript/Scripts/Exercise5Script.js
@@ -0,0 +1,10 @@
+function calculate() {
+  let x = prompt("Въведете х: ");
+  let y = prompt("Въведете у: ");
+
+  let num1 = parseInt(x);
+  let num2 = parseInt(y);
+
+  let result = num1 + num2;
+  document.write(result);
+}
