3b6175d7af8758db2dadfb60119cef0e5bf5bdc3
Martin Marinov <m.marinov1733@gmail.com>
2025-08-18 09:56:48 +0300

    описание

--- Урок 12 - Въведение в JavaScript/Scripts/Exercise9Script.js
+++ Урок 12 - Въведение в JavaScript/Scripts/Exercise9Script.js
@@ -1,15 +1,31 @@
 const martin = {
   firstName: "Мартин",
-  age: 20,
+  birthYear: 2004,
+  calculateAge(birthYear) {
+    return 2025 - birthYear;
+  },
   town: "Горна Оряховица",
   job: "програмист",
   projects: ["Mansor", "Indigo", "Onyx"],
 };
 
 const display = function (person) {
-  const result = `Казвам се ${person.firstName} и съм на ${person.age} години. От град ${person.town} съм. 
+  const result = `Казвам се ${person.firstName} и съм на ${person.calculateAge(
+    person.birthYear
+  )} години. От град ${person.town} съм. 
   Искам да стана ${person.job}. Моите проекти са ${person.projects}`;
   return result;
 };
 
+function isAdult(age) {
+  if (age >= 18) {
+    console.log("Пълнолетен си");
+  } else if (age < 18) {
+    console.log(`Остават ти ${18 - age} години до пълнолетие`);
+  } else {
+    console.log("Невалидни данни");
+  }
+}
+
 console.log(display(martin));
+isAdult(martin.calculateAge(martin.birthYear));
